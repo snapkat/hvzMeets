c31047fe641e42bf3382913b71ea3cac44f775e8
Annie Zhang <anniey.zhang@mail.utoronto.ca>
2016-07-27 16:11:59 -0700

    Update hvzMeets.py
    Added num players that can attend one meeting

--- hvzMeets.py
+++ hvzMeets.py
@@ -17,8 +17,7 @@ def findBestTimeSlots(day1, day2):
         avail_a = findAvailPlayers(day1, a)
         for b in range(len(day2[0])):
             avail_b = findAvailPlayers(day2, b)
-            in_a_not_b = set(avail_a) - set(avail_b)
-            avail = list(in_a_not_b) + avail_b
+            avail = set(avail_a).union(set(avail_b))
             if len(avail) > max_peeps:
                 max_peeps = len(avail)
                 best_set = avail
@@ -28,23 +27,23 @@ def findBestTimeSlots(day1, day2):
 
 
 def main():
-    file = open('Doodle.csv')
-    times = ["10AM", "11AM", "1PM", "2PM", "3PM", "4PM", "5PM"]
-    players = []
-    mon = []
-    tue = []
-    wed = []
-    thur = []
-    fri = []
-    for line in file:
-        line = line.strip().split(",")
-        assert len(line) == 36, "Bad line length at for user %s" % line[0]
-        players.append(line[0])
-        mon.append(line[1:8])
-        tue.append(line[8:15])
-        wed.append(line[15:22])
-        thur.append(line[22:29])
-        fri.append(line[29:])
+    with open('/tmp/Doodle.csv') as file:
+        times = ["10AM", "11AM", "1PM", "2PM", "3PM", "4PM", "5PM"]
+        players = []
+        mon = []
+        tue = []
+        wed = []
+        thur = []
+        fri = []
+        for line in file:
+            line = line.strip().split(",")
+            assert len(line) == 36, "Bad line length at for user %s" % line[0]
+            players.append(line[0])
+            mon.append(line[1:8])
+            tue.append(line[8:15])
+            wed.append(line[15:22])
+            thur.append(line[22:29])
+            fri.append(line[29:])
 
     times_m_t, players_m_t = findBestTimeSlots(mon, tue)
     times_w_t, players_w_t = findBestTimeSlots(wed, thur)
@@ -52,15 +51,17 @@ def main():
     fri_count = max(map(int, fri[-1]))
     slot = fri[-1].index(str(fri_count))
     friday_players = findAvailPlayers(fri, slot)
-    #avail = findAvail(fri, max())
+
     print("Mon: %s Tues: %s" % (times[times_m_t[0]], times[times_m_t[1]]))
     print("Unique players: %s" % len(players_m_t))
     print("Wed: %s Thurs: %s" % (times[times_w_t[0]], times[times_w_t[1]]))
     print("Unique players: %s" % len(players_w_t))
     print("Fri: %s" % times[slot])
     print("Unique players: %d" % fri_count)
-    attend_all = set(players_m_t).intersection(set(players_w_t), set(friday_players))
+    attend_all = players_m_t.intersection(players_w_t, set(friday_players))
+    attend_one = players_m_t.union(players_w_t, set(friday_players))
     print("Number players that can attend all: %d" % len(attend_all))
+    print("Number players that can attend one: %d" % len(attend_one))
 
 if __name__ == "__main__":
     main()
